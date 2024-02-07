548a928f4a8494e9dd9ba45f82af4f8e5deac397
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-02-08 00:55:08 +0600

    task5

--- problem1.js
+++ problem1.js
@@ -1,3 +1,4 @@
+// 1
 // function calculateMoney(ticketSale) {
 //     if (ticketSale <= 0) {
 //         return 'Invalid Number'
@@ -11,9 +12,9 @@
 //     return totalTk
 
 // }
-// const ticket =10;
+// const ticket =-130;
 // console.log(calculateMoney(ticket));
-
+// 2
 // function checkName(name) {
 //   if (typeof name !== "string") {
 //     return "invalid";
@@ -34,15 +35,9 @@
 //     return "Bad Name";
 //   }
 // }
-
 // const myName = "utsho";
 // console.log(checkName(myName));
-
-
-
-
-
-
+//3 
 // function deleteInvalids(array) {
 //   let myStore = [];
 //   if (Array.isArray(array) !== true) {
@@ -59,77 +54,62 @@
 // const myInput = [ 1 , 2 , -3 ];
 // console.log(deleteInvalids(myInput));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// 4
 /*function signature/sample */
-function monthlySavings(arr , livingCost) {
-
-    // You have to write your code here
+function password(obj) {
+    if (Object.keys(obj).length !== 3) {
+        return "invalid"
     }
+    else if (!obj.birthYear || obj.birthYear.toString().length < 4) {
+        return "invalid"
+    }
+    else {
+    let name = obj.name;
+    let birth = obj.birthYear;
+    let siteName = obj.siteName;
+    let slicing= siteName.slice(1);
+    let toUpperCase =siteName.charAt(0).toUpperCase();
+    let addition =toUpperCase+slicing;
+    let myOutput =`${addition}#${name}@${birth}`
+    return myOutput;
+    }
+    
+}
+const myArray = { name: 'maisha' , birthYear: 2002 };
+console.log(password(myArray));
+
+
+
+
+
+
+
+
+
+
+// 5
+
+// function monthlySavings(arr, livingCost) {
+//   let sum = 0;
+
+//   if (Array.isArray(arr) === false || typeof livingCost !== "number") {
+//     return "invalid input";
+//   }
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] >= 3000) {
+//       let discount = 0.2 * arr[i];
+//       arr[i] -= discount;
+//     }
+//     sum = sum + arr[i];
+//   }
+//   let result = sum - livingCost;
+//   if (result < 0) {
+//     return "earn more";
+//   }
+//   return result;
+// }
+
+// const myarray = [1000, 2000, 3000];
+// const livingCost = 5400;
+// console.log(monthlySavings(myarray, livingCost));
