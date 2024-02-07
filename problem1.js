54154dbdabc4a223970a22c02cef189bed8e5e0d
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-02-07 20:42:16 +0600

    task1

---
+++ problem1.js
@@ -0,0 +1,34 @@
+// function calculateMoney(ticketSale) {
+//     if (ticketSale <= 0) {
+//         return 'Invalid Number'
+//     }
+//     let ticketPrice = 120;
+//     let perDayDarowan = 500;
+//     let stupLunce = 8 * 50;
+//     let totalDarwanAndLunce = perDayDarowan + stupLunce;
+//     let totalTicketSellPrice = ticketPrice * ticketSale;
+//     let totalTk = totalTicketSellPrice -  totalDarwanAndLunce;
+//     return totalTk
+
+// }
+// const ticket =10;
+// console.log(calculateMoney(ticket));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
