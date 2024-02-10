a8796b48394901c3ca5d48a81a41292c4c9ff7a5
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-02-10 23:12:55 +0600

    task

--- demo.js
+++ demo.js
@@ -114,3 +114,42 @@ const chinkusMarks = 99;
 
 const theCakeWillGet = findingTopper(jimsMarks, delasMarks, chinkusMarks);
 console.log(theCakeWillGet);
+
+
+/*
+
+## Practice Problem 45
+
+**Write an arrow function where it will do the following:**
+
+1. It will take an array where the array elements will be the
+name of your friends
+
+2. Check if the length of each element is even, push elements
+with even length to a new array and return the result
+Print the result.
+
+
+
+*/
+
+
+const findEven = nameList => {
+    
+    const evenLengthName = [];
+    for(let i = 0; i < nameList.length; i++){
+
+        const friendName = nameList[i];
+        if(friendName.length % 2 == 0){
+            evenLengthName.push(friendName);
+        }
+    }
+   
+    return evenLengthName;
+}
+
+
+const friendsName = ["Shourav", "Nirob", "Alal", "Jala", "Ashim", "Shipta", "Pritom", "Arko", "Alu"];
+
+const evenLengthFriendsNameList = findEven(friendsName);
+console.log(evenLengthFriendsNameList);
